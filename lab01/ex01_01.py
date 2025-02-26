75f882bfa11a8f35e5059a819b7bbbb68c2be1a8
Cookiescooker <fhhdhysd@gmail.com>
2025-02-26 14:16:30 +0700

    Update

--- lab01/ex01_01.py
+++ lab01/ex01_01.py
@@ -1,4 +1,4 @@
-ten = input ('Nhap ten cua ban: ')
-tuoi = int("Nhap tuoi cua ban")
+ten = input('Nhap ten cua ban: ')
+tuoi = int(input("Nhap tuoi cua ban: "))
 
 print(f"Chào mừng,", ten, "! Bạn", tuoi, "tuổi")
---
+++ lab01/ex01_02.py
@@ -0,0 +1,4 @@
+radius = float(input("Nhập bán kính đường tròn: "))
+size = 3.14 * (radius ** 2)
+
+print("Diện tích đường tròn: ", size)
