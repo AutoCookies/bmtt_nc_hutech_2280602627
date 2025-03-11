b7754242810b68b1ffe5d3bf6a92cfc8bc2411be
Cookiescooker <fhhdhysd@gmail.com>
2025-03-11 23:34:08 +0700

    Update

--- lab02/api.py
+++ lab02/api.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 from cypher.ceasar import CeasarCipher
 from cypher.vigenere import VigenereCipher
 from cypher.railfence import RailFenceCipher
+from cypher.transposition import TranspositionCipher
 
 app = Flask(__name__)
 
 ceasar_cipher = CeasarCipher()
 vigenere_cipher = VigenereCipher()
 railfence = RailFenceCipher()
+transposition_cipher = TranspositionCipher()
 
 @app.route("/api/ceasar/encrypt", methods=['POST'])
 def ceasar_encrypt():
@@ -56,6 +58,25 @@ def railfence_decrypt():
     key = int(data['key'])
     decrypted_text = railfence.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
