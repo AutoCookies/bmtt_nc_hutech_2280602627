a02bcf615abc045aa332ddcfa427af1fd3bfd0c4
AutoCookies <fhhdhysd@gmail.com>
2025-03-26 13:49:03 +0700

    Update

--- lab03/api.py
+++ lab03/api.py
@@ -70,7 +70,7 @@ def rsa_verify_signature():
 @app.route('/api/ecc/generate_keys', methods=['GET'])
 def ecc_generate_keys():
     ecc_cipher.generate_keys()
-    return jsonify({'message': "Key generated Successfully"})
+    return jsonify({'message': 'Key generated successfully'})
 
 @app.route('/api/ecc/sign', methods=['POST'])
 def ecc_sign_message():
@@ -79,17 +79,17 @@ def ecc_sign_message():
     private_key, _ = ecc_cipher.load_keys()
     signature = ecc_cipher.sign(message, private_key)
     signature_hex = signature.hex()
-    return jsonify({'signature': signature_hex})
+    return jsonify({"signature": signature_hex})
 
-@app.route('/api/ecc/verify', methods = ['POST'])
+@app.route('/api/ecc/verify', methods=['POST'])
 def ecc_verify_signature():
-    data = request.json()
+    data = request.json
     message = data['message']
-    signature_hex = data['signature']
+    signature_hex = data["signature"]
     public_key, _ = ecc_cipher.load_keys() 
     signature = bytes.fromhex(signature_hex)
     is_verified = ecc_cipher.verify(message, signature, public_key)
-    return jsonify({"is_verified: ", is_verified})
+    return jsonify({"is_verified": is_verified})
 
 # Main function
 if __name__ == "__main__":
--- lab03/cipher/ecc/ecc_cipher.py
+++ lab03/cipher/ecc/ecc_cipher.py
@@ -6,39 +6,35 @@ if not os.path.exists('cipher/ecc/keys'):
     os.makedirs("cipher/ecc/keys", exist_ok=True)
 
 class ECCCipher:
-    def __init__(self):
+    def __init__ (self):
         pass
 
-    def generate_keys(self):
+    def generate_keys (self):
         sk = ecdsa.SigningKey.generate()
         vk = sk.get_verifying_key()
 
-        # Save private key
         with open('cipher/ecc/keys/privateKey.pem', 'wb') as p:
             p.write(sk.to_pem())
         
-        # Save public key
         with open('cipher/ecc/keys/publicKey.pem', 'wb') as p:
             p.write(vk.to_pem())
 
-    def load_keys(self):
-        # Load private key
+    def load_keys (self):
         with open('cipher/ecc/keys/privateKey.pem', 'rb') as p:
             sk = ecdsa.SigningKey.from_pem(p.read())
 
-        # Load public key
         with open('cipher/ecc/keys/publicKey.pem', 'rb') as p:
             vk = ecdsa.VerifyingKey.from_pem(p.read())
     
         return sk, vk
 
-    def sign(self, message, key):
-        # Sign a message with the provided signing key
+    def sign (self, message, key):
         return key.sign(message.encode('ascii'))
 
     def verify(self, message, signature, key):
-        # Verify the signature using the provided public key
+        _, vk = self.load_keys()
         try:
-            return key.verify(signature, message.encode('ascii'))
+            return vk.verify(signature, message.encode('ascii'))
         except ecdsa.BadSignatureError:
             return False
+            
